12c0d4c38cd6be8c6eaf0aa7dabd3b0cdf06930f
Do-then-quit <redfowl@naver.com>
2021-01-05 20:58:55 +0900

    animationView로 독립!

--- animate/ContentView.swift
+++ animate/ContentView.swift
@@ -6,49 +6,14 @@
 //
 
 import SwiftUI
-import Combine
-
-class LoadingTimer {
-    let publisher = Timer.publish(every: 1 / 30, on: .main, in: .default)
-    private var timerCancellable: Cancellable?
-
-    func start() {
-        self.timerCancellable = publisher.connect()
-    }
-
-    func cancel() {
-        self.timerCancellable?.cancel()
-    }
-}
 
 struct ContentView: View {
-    @State private var index = 0
 
-    private let images = (0...142).map { UIImage(named: String(format: "hiyori_m01/hiyori_m01_" + "%03d", $0))! }
-   
-    
-    private var timer = LoadingTimer()
-    
     var body: some View {
         VStack {
             Text("Hello, world!")
-            Image(uiImage: images[index])
-                .resizable()
-                .onReceive(
-                    timer.publisher,
-                    perform: { _ in
-                        self.index = self.index + 1
-                        if self.index >= 142 {
-                            self.index = 141
-                        }
-                        
-                    }
-                )
-                .onAppear { self.timer.start() }
-                .onDisappear { self.timer.cancel() }
-                .onTapGesture(perform: {
-                    self.index = 0
-                })
+            animationView()
+            
         }
     }
 }
---
+++ animate/animationView.swift
@@ -0,0 +1,53 @@
+//
+//  animation.swift
+//  animate
+//
+//  Created by LeeMinkyo on 2021/01/05.
+//
+
+import SwiftUI
+import Combine
+
+class LoadingTimer {
+    let publisher = Timer.publish(every: 1 / 30, on: .main, in: .default)
+    private var timerCancellable: Cancellable?
+
+    func start() {
+        self.timerCancellable = publisher.connect()
+    }
+
+    func cancel() {
+        self.timerCancellable?.cancel()
+    }
+}
+
+struct animationView: View {
+    @State private var index = 0
+    private let images = (0...142).map { UIImage(named: String(format: "hiyori_m01/hiyori_m01_" + "%03d", $0))! }
+    private var timer = LoadingTimer()
+    
+    var body: some View {
+        Image(uiImage: images[index])
+            .resizable()
+            .onReceive(
+                timer.publisher,
+                perform: { _ in
+                    self.index = self.index + 1
+                    if self.index >= 142 {
+                        self.index = 141
+                    }
+                }
+            )
+            .onAppear { self.timer.start() }
+            .onDisappear { self.timer.cancel() }
+            .onTapGesture(perform: {
+                self.index = 0
+            })
+    }
+}
+
+struct animation_Previews: PreviewProvider {
+    static var previews: some View {
+        animationView()
+    }
+}
